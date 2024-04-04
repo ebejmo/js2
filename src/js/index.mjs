9e25c1087250c556a0914a78cc50dde9b1e4edf9
ebejmo <94139531+ebejmo@users.noreply.github.com>
2024-04-04 17:01:11 +0200

    eventhandler

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,1 +1,6 @@
-console.log("Hello");
+import { API_BASE_URL } from "./api/constants.mjs";
+import { API_AUTH } from "./api/constants.mjs";
+import { API_REGISTER } from "./api/constants.mjs";
+import { formHandler } from "./eventhandlers/registerForm.mjs";
+
+formHandler();
