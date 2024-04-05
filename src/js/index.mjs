cf980a10d55219546d6b630179fec227a0a00bdb
ebejmo <94139531+ebejmo@users.noreply.github.com>
2024-04-05 13:31:45 +0200

    pathname

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,6 +1,12 @@
 import { API_BASE_URL } from "./api/constants.mjs";
 import { API_AUTH } from "./api/constants.mjs";
 import { API_REGISTER } from "./api/constants.mjs";
-import { formHandler } from "./eventhandlers/registerForm.mjs";
+import { registerFormHandler } from "./eventhandlers/registerForm.mjs";
 
-formHandler();
+const path = location.pathname;
+
+if (path === "index.html") {
+  //   loginFormHandler();
+} else if (path === "/profile/") {
+  registerFormHandler();
+}
