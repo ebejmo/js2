17fe775de0d4308d93ea11818af2c3c3f53f52a8
ebejmo <94139531+ebejmo@users.noreply.github.com>
2024-04-08 12:44:55 +0200

    login event handler function

--- src/js/eventhandlers/loginForm.mjs
+++ src/js/eventhandlers/loginForm.mjs
@@ -1,1 +1,23 @@
+import { loginUser } from "../api/auth/loginUser.mjs";
 
+export function loginFormHandler() {
+  const form = document.querySelector("#loginForm");
+
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const profile = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    try {
+      const response = await loginUser(profile);
+
+      console.log("Login response FORM:", response);
+    } catch (error) {
+      console.log("ERROR! Login failed", error);
+    }
+  });
+}
