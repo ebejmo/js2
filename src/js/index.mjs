3d9dbdc875ac8bfc8b39e15b4e54ac8344f498bb
ebejmo <94139531+ebejmo@users.noreply.github.com>
2024-04-09 16:09:35 +0200

    getPost path

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -2,8 +2,11 @@ import { API_BASE_URL, API_SOCIAL, POSTS } from "./api/constants.mjs";
 import { registerFormHandler } from "./eventhandlers/registerForm.mjs";
 import { loginFormHandler } from "./eventhandlers/loginForm.mjs";
 import { getPosts } from "./api/posts/getPosts.mjs";
+import { getPost, id } from "./api/post/getPost.mjs";
 
 const path = location.pathname;
+
+console.log(id);
 
 if (path === "/index.html") {
   loginFormHandler();
@@ -11,4 +14,6 @@ if (path === "/index.html") {
   registerFormHandler();
 } else if (path === "/feed/posts/") {
   getPosts(API_BASE_URL + API_SOCIAL + POSTS);
+} else if (path === "/feed/posts/post/") {
+  getPost(`${API_BASE_URL}${API_SOCIAL}${POSTS}/${id}`);
 }
