e9246f1061ccbac7dd5c9cfb2422e98cb197c58f
ebejmo <94139531+ebejmo@users.noreply.github.com>
2024-04-09 13:15:56 +0200

    dynamically generating and render html with styles

--- src/js/api/posts/renderPosts.mjs
+++ src/js/api/posts/renderPosts.mjs
@@ -1,33 +1,63 @@
 export function renderPosts(content) {
-  const postsContainer = document.querySelector("#posts");
+  const contentContainer = document.querySelector("#content");
+
+  // clear content without using innerHTML
+  while (contentContainer.firstChild) {
+    contentContainer.removeChild(contentContainer.firstChild);
+  }
 
   content.data.forEach(({ id, title, body, media }) => {
-    const postElement = document.createElement("div");
-    postElement.classList.add("post");
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card", "mb-3", "h-100");
+    cardContainer.id = id;
 
-    const titleElement = document.createElement("h2");
-    titleElement.textContent = title;
+    const row = document.createElement("div");
+    row.classList.add("row", "g-0");
+    cardContainer.appendChild(row);
 
-    const bodyElement = document.createElement("p");
-    bodyElement.textContent = body;
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add(
+      "d-none",
+      "d-sm-block",
+      "col-4",
+      "col-md-3",
+      "col-lg-4",
+      "h-100"
+    );
+    row.appendChild(imgContainer);
 
-    const idElement = document.createElement("p");
-    idElement.textContent = `ID: ${id}`;
+    const img = document.createElement("img");
+    img.classList.add("img-fluid", "w-100", "h-100");
+    img.src = media
+      ? media.url
+      : "https://placehold.co/400?text=Noroff+Social&font=roboto";
+    img.alt = media ? media.alt : "Placeholder image";
+    imgContainer.appendChild(img);
 
-    const imageElement = document.createElement("img");
-    if (media) {
-      const { url, alt } = media;
-      imageElement.src = url;
-      imageElement.alt = alt;
-    }
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("col-12", "col-sm-8", "col-md-9", "col-lg-8");
+    row.appendChild(textContainer);
 
-    postElement.appendChild(titleElement);
-    postElement.appendChild(bodyElement);
-    postElement.appendChild(idElement);
-    if (media) {
-      postElement.appendChild(imageElement);
-    }
+    const header = document.createElement("h5");
+    header.classList.add("card-header");
+    header.textContent = title;
+    textContainer.appendChild(header);
 
-    postsContainer.appendChild(postElement);
+    const bodyContainer = document.createElement("div");
+    bodyContainer.classList.add("card-body");
+    textContainer.appendChild(bodyContainer);
+
+    const cardText = document.createElement("p");
+    cardText.classList.add("card-text");
+    cardText.textContent = body;
+    bodyContainer.appendChild(cardText);
+
+    const link = document.createElement("a");
+    link.classList.add("primary");
+    link.href = "#";
+    link.textContent = "Read more";
+    bodyContainer.appendChild(link);
+
+    contentContainer.appendChild(cardContainer);
   });
 }
