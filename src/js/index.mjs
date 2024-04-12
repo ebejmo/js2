ded5790795d90e15525221e1ea7b16323e7e49a0
ebejmo <94139531+ebejmo@users.noreply.github.com>
2024-04-12 11:53:59 +0200

    hide form styles and eventListener to enable the form on click

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -4,16 +4,20 @@ import { loginFormHandler } from "./eventhandlers/loginForm.mjs";
 import { getPosts } from "./api/posts/getPosts.mjs";
 import { getPost, id } from "./api/post/getPost.mjs";
 import { attachCreatePostFormListener } from "./eventhandlers/attachCreatePostFormListener.mjs";
+import { toggleFormShow } from "./eventhandlers/updatePostForm/toogleFormShow.mjs";
 
 const path = location.pathname;
+const params = new URLSearchParams(window.location.search);
+const postId = params.get("id");
 
 if (path === "/index.html") {
   loginFormHandler();
 } else if (path === "/register/") {
   registerFormHandler();
 } else if (path === "/feed/posts/") {
-  getPosts(API_BASE_URL + API_SOCIAL + POSTS + DETAILS);
+  await getPosts(API_BASE_URL + API_SOCIAL + POSTS + DETAILS);
   attachCreatePostFormListener();
 } else if (path === "/feed/posts/post/") {
   await getPost(`${API_BASE_URL}${API_SOCIAL}${POSTS}/${id}${DETAILS}`);
+  toggleFormShow();
 }
