af6ee269cceae599aba5862d162ae8fcb06768fe
ebejmo <94139531+ebejmo@users.noreply.github.com>
2024-04-24 12:46:39 +0200

    test

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -10,6 +10,7 @@ import { setupEditButton } from "./eventhandlers/updatePostForm/setupEdit.mjs";
 import { setupSearchForm } from "./api/search/setupSearch.mjs";
 
 const path = location.pathname;
+console.log("hello");
 
 if (path === "/index.html") {
   loginFormHandler();
