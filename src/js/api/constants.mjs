8075cc4144d17f4c08be4f5e8fa2a5065dcb59ce
ebejmo <94139531+ebejmo@users.noreply.github.com>
2024-05-06 21:06:50 +0200

    removing files from main branch. accidental push to main in early april

--- src/js/api/constants.mjs
+++
@@ -1,8 +0,0 @@
-export const API_BASE_URL = "https://v2.api.noroff.dev";
-export const API_AUTH = "/auth";
-export const API_REGISTER = "/register";
-export const API_LOGIN = "/login";
-export const API_CREATE_KEY = "/create-api-key";
-export const API_KEY = "15068765-9d50-4895-b37f-8df955555c36";
-export const API_SOCIAL = "/social";
-export const POSTS = "/posts";
